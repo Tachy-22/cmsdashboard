dee11c76bea725284d6fc05ce46380e4d60eddfc
Tachy-22 <entekumejeffrey@gmail.com>
2024-03-09 11:12:24 +0100

    update for mobile layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -5,7 +5,7 @@ const DashBoardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className=" flex h-full fixed w-full ">
       <SideMenu />
-      <div className="flex flex-col w-full h-full min-h-full border border-red-500 justify-start items-center  ">
+      <div className="flex flex-col w-full h-full min-h-full justify-start items-center  ">
         {children}
       </div>
     </div>
